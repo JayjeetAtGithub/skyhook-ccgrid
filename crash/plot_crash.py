132e6f387677ec132e3257bf9876166f81acc31b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-27 19:21:41 -0800

    New crash plot

--- crash/plot_crash.py
+++ crash/plot_crash.py
@@ -2,11 +2,12 @@ from datetime import time
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from matplotlib.pyplot import figure
+
 
 if __name__ == "__main__":
-    ax = figure(figsize=(15, 12), dpi=80)
-    sns.set_palette('Greys', n_colors=1)
+    # sns.set_style('whitegrid')
+    ax = plt.figure(figsize=(15, 12), dpi=80)
+    sns.set_palette('Set2', n_colors=1)
     df = pd.read_csv('crash.csv')
     rows = len(df)
 
@@ -20,10 +21,11 @@ if __name__ == "__main__":
 
     df['Time'] = timestamp
     df['Throughput'] = th
-    sns_plot = sns.lineplot(data=df, x="Time", y="Throughput", linewidth=2.5)
+    sns_plot = sns.lineplot(data=df, x="Time", y="Throughput", color='black', linewidth=3)
     sns_plot.set(ylim=(0, 1050))
     sns_plot.set_ylabel('Throughput (MBps)', fontsize=32)
     sns_plot.set_xlabel('Timestamp (s)', fontsize=32)
     sns_plot.tick_params(axis="y", labelsize=32)
     sns_plot.tick_params(axis="x", labelsize=32)
-    plt.savefig('crash.png', bbox_inches='tight')
+    sns_plot.grid(axis='y')
+    plt.savefig('crash.pdf', bbox_inches='tight')
